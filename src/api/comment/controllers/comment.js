d16793deabb0e615bf353ecb03bcefec9c4de197
Henus321 <tyrant321@yandex.ru>
2023-03-21 23:52:10 +0300

    feat(control): custom create method added to comment controller

--- src/api/comment/controllers/comment.js
+++ src/api/comment/controllers/comment.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * comment controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::comment.comment');
+module.exports = createCoreController("api::comment.comment", ({ strapi }) => ({
+  async create(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+    const response = await super.create(ctx);
+    const updatedResponse = await strapi.entityService.update(
+      "api::comment.comment",
+      response.data.id,
+      { data: { user: user.id } }
+    );
+    return updatedResponse;
+  },
+}));
